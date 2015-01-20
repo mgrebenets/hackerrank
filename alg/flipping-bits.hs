3c10ba920b5bccf18b000a24055279e06a291be6
maksym.grebenets <maksym.grebenets@fairfaxmedia.com.au>
2015-01-20 17:08:29 +1100

    Initial commit

---
+++ alg/flipping-bits.hs
@@ -0,0 +1,11 @@
+-- Enter your code here. Read input from STDIN. Print output to STDOUT
+import Control.Monad
+import Data.Bits
+import Data.Word
+
+main :: IO ()
+main = do
+  n <- readLn :: IO Int
+  arr <- replicateM n getLine
+  let ans = map complement ((map read arr) :: [Word32])
+  mapM_ print ans
---
+++ alg/solve-me-second.sh
@@ -0,0 +1,9 @@
+import Control.Monad
+
+main :: IO ()
+main = do
+  n <- readLn :: IO Int
+  str <- replicateM n getLine
+  let
+    ans = map (sum. map read. words) str
+  mapM_ print ans
