e41bb89f0aff0301f7ae45ed24e38ca34e60d6de
maksym.grebenets <maksym.grebenets@fairfaxmedia.com.au>
2015-01-29 22:35:19 +1100

    ACM ICPC

--- alg/angry-children.hs
+++ alg/angry-children.hs
@@ -1,6 +1,7 @@
 -- Enter your code here. Read input from STDIN. Print output to STDOUT
 import Control.Monad
 import Data.List
+import Data.Array
 --import Data.Sequence (unstableSort, fromList)
 --import Data.Foldable (toList)
 
@@ -28,16 +29,26 @@ import Data.List
 --unfairness slice = maximum slice - minimum slice
 
 
-foldMin :: ([Int], Int, Int, Int) -> Int
-foldMin (list, s, k, m) = if s >= k - 1
-  then foldMin(list, s - 1, k, minimum (list!!s - list!!(s - k + 1)), m) else m
-
 --slice xs s k = take k . drop s $ xs
 --slice xs s k = take k $ drop s xs
 --slice :: Int -> Int -> [Int]-> [Int]
 --slice xs s k = take k (drop s xs)
 
+foldMin :: ([Int], Int, Int, Int) -> Int
+foldMin (list, s, k, m) = if s >= k - 1
+  then foldMin(list, s - 1, k, minimum [list!!s - list!!(s - k + 1), m]) else m
+
+foldMin2 (list, s, k, m )
+  | s >= k - 1 = foldMin2(list, s - 1, k, min2)
+  | otherwise = m
+  where
+    comp = list!!s - list!!(s - k + 1)
+    min2 = if comp < m then comp else m
+
+
 angryKids :: Int -> [Int] -> Int
+--angryKids :: Int -> (Array Int Int) -> Int
+
 --angryKids k list = minimum [unfairness (slice list s k) | s <- [0..((length list) - k)] ]
 --angryKids k list = minimum [list!!(s + k - 1) - list!!s | s <- [0..(length list - k)]]
 --angryKids k list = (sort ([list!!(s + k - 1) - list!!s | s <- [0..(length list - k)]]))!!0
@@ -46,14 +57,24 @@ angryKids :: Int -> [Int] -> Int
 -- it shows that minimum as well as sort is the problem here
 --angryKids k list = ([list!!(s + k - 1) - list!!s | s <- [0..(length list - k)]])!!0
 
-angryKids k list = minimum [list!!(s + k - 1) - list!!s | s <- [0..(length list - k)]]
+--angryKids k list = foldMin(list, length list - 1, k, last list)
+--angryKids k list = foldMin2(list, length list - 1, k, last list)
+angryKids k list = minimum [array!(s + k - 1) - array!s | s <- [0..(len - k)]]
+  where
+    array = listArray (0, len - 1) (sort list)
+    len = length list
 
--- angryKids k list = foldMin(list, length list - 1, k, last list)
+  --angryKids k list = minimum [list!!(s + k - 1) - list!!s | s <- [0..(length list - k)]]
+
+  --op :: Int -> [Int]
+  --anrgyKids k list = foldl' (op) (last list) list
 
 main :: IO ()
 main = do
   n <- readLn :: IO Int
   k <- readLn :: IO Int
-  arr <- replicateM n readLn :: IO [Int]
-  let ans = angryKids k (sort arr)
-  print ans
+  list <- replicateM n readLn :: IO [Int]
+  --  let array = listArray (0, length list - 1) (sort list) :: Array Int Int
+  let ans = angryKids k list
+  --  print ans
+  print  ans
