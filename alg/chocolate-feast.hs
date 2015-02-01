201afbf43711e4540c6a5fefaed53d56e4f4b862
maksym.grebenets <maksym.grebenets@fairfaxmedia.com.au>
2015-02-02 00:29:49 +1100

    Choc feast'

--- alg/chocolate-feast.hs
+++ alg/chocolate-feast.hs
@@ -1,11 +1,27 @@
 -- Enter your code here. Read input from STDIN. Print output to STDOUT
 import Control.Monad
 
-chocolates xs = 1
+-- sample: 6 3 5
+
+readNumbers :: String -> [Int]
+readNumbers = map read . words
+
+readInputs [n, c, m] = (n, c, m)
+
+buy = div
+
+exchange w m
+  | w == 0 = 0
+  | w < m = 0
+  | otherwise = chocs + exchange (chocs + mod w m) m where chocs = div w m
+
+feast (n, c, m) = chocs + exchange chocs m where chocs = buy n c
 
 main :: IO ()
 main = do
   n <- readLn :: IO Int
-  list <- replicateM n readLn :: IO [Int]
-  let ans = chocolates list
-  print  ans
+  list <- replicateM n getLine
+  let inputs = map (readInputs . readNumbers) list
+  -- print inputs
+  let ans = map feast inputs
+  mapM_ print ans
