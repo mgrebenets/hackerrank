7dd71db5f16f5474abce7d2f5b9724a81a173fa7
maksym.grebenets <maksym.grebenets@fairfaxmedia.com.au>
2015-03-17 22:33:01 +1100

    Fixup

--- alg/warmup/solve-me-first.swift
+++ alg/warmup/solve-me-first.swift
@@ -1,22 +1,6 @@
 /// Solve me first
 /// https://www.hackerrank.com/challenges/solve-me-first
 public func solveMeFirst() {
-// getLine returns as string
-// readLn - gets a string then reads (converts) to a given type
-// TODO: have a look at haskel stdin to see what's the core set of methods
-
-// most often used: readLn, getLine, read
-// readLn without convertion should be just like getLine, but have a look at the sum example
-// it sums two ints, how?
-
-// have a look at treplicateM, should the same be used here?
-// this will allow to read custom input
-// not just COUNT:[ARR] format,but COUNT1:[ARR1] COUNT2:[ARR2] and so on
-
-// and finally mapM_ what's with that thing?
-///
-///:param: max specifies the number of bytes to read
-///:returns: the string, or nil if an error was encountered trying to read Stdin
     let a: Int = readLn()
     let b: Int = readLn()
     println(a + b)
