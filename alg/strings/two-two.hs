f80cd264bab32e602ab1faa6e69023abb3a3e3ed
maksym.grebenets <maksym.grebenets@fairfaxmedia.com.au>
2015-04-07 23:28:53 +1000

    Two two with use of prefixes.

--- alg/strings/two-two.hs
+++ alg/strings/two-two.hs
@@ -1,35 +1,21 @@
 import Control.Monad
 import qualified Data.Vector as V
-import Data.Char
-import Data.Bits
+import Data.List
 
--- isPowerOfTwo n = if popCount n == 1 && testBit (complement n) 0 then 1 else 0
-isPowerOfTwo n = 0
+powers m = map (2^) [0..m]
 
-strength v
-    | V.length v == 0 = 0
-    | V.head v == '0' = 0
-    | otherwise = fullStrength
-        where
-        -- TODO: no need to fold, just convert int to string
-            fullStrength = V.foldl' (\acc c -> acc * 10 + digitToInt c) 0 v
-
-countPowers v (i, j) = strength s
+twotwo ps acc [] = acc
+twotwo ps acc l@(x:xs) = twotwo ps newAcc xs
     where
-        s = V.slice i (j - i + 1) v
-
--- TODO: use the fact that max strength is the slice number itself
-
-twotwo s = [countPowers v (i, j) | i <- [0..len - 1], j <- [i..len - 1], 4 * (j - i + 1) < 800]
-    where
-        v = V.fromList s
-        len = length v
+        newAcc = acc + foldl' (\acc p -> if p `isPrefixOf` l then acc + 1 else acc) 0 ps
 
 main :: IO ()
 main = do
     n <- readLn :: IO Int
     testCases <- replicateM n getLine
     -- print testCases
-    let ans = map twotwo testCases
-    -- print ans
-    mapM_ (print . sum) ans
+    let maxPower = 800
+    let powerStrings = map show (powers maxPower)
+    let ans = map (twotwo powerStrings 0) testCases
+    print ans
+    -- mapM_ (print . sum) ans
